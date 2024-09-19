da718b7a3270d28d5ddfe278bd8ae5cc01840942
Choaib ELMADI <choaibamd@gmail.com>
2024-09-19 10:17:02 +0100

    Fix ModelSim issues when running VHDL programs

---
+++ Projects/tb_0_hello_world.vhd
@@ -0,0 +1,15 @@
+entity tb_0_hello_world is
+end entity;
+
+architecture sim of tb_0_hello_world is
+begin
+
+    process is
+    begin
+
+        report "Hello, World! YEAAAH";
+        wait;
+
+    end process;
+
+end architecture;
