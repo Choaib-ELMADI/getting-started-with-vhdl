25ab8ca1e4cbec5ff4ee45e9b440a556a13b8290
Choaib ELMADI <choaibamd@gmail.com>
2024-09-26 21:50:24 +0100

    Update docs and notes

---
+++ Docs/wait/wait on - wait until/wait_on_wait_until_tb.vhd
@@ -0,0 +1,15 @@
+entity wait_on_wait_until_tb is
+end entity;
+
+architecture sim of wait_on_wait_until_tb is
+begin
+
+    process is
+    begin
+
+        
+        
+
+    end process;
+
+end architecture;
